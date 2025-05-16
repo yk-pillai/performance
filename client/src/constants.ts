6b2fcf3637913d4f1f66e15523323e65f50e7223
Yedhukrishnan Pillai <yedhu.pillai3@gmail.com>
2025-05-16 11:38:39 +0530

    domain url changed

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -5,3 +5,5 @@ export const FETCH_MULTIPLYER = 5
 export const ANY = '00000000-0000-0000-0000-000000000000'
 export const API_BACKEND_URL = 'https://api.yedhukrishnan.com/api'
 export const ASSETS_BACKEND_URL = 'https://api.yedhukrishnan.com'
+// export const API_BACKEND_URL = 'http://localhost:5000/api'
+// export const ASSETS_BACKEND_URL = 'http://localhost:5000'
--- server/index.ts
+++ server/index.ts
@@ -28,7 +28,7 @@ async function startServer() {
         "http://localhost:3000",
         "http://127.0.0.1:8080",
         "http://localhost:8080",
-        "https://yedhukrishnan.com",
+        "https://performance.yedhukrishnan.com",
       ],
       credentials: true,
     })
